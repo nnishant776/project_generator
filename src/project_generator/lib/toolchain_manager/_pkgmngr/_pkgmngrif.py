7a90903da0aedf8208789fe5fca6b4c7804b6a0b
Neeraj Nishant <nn54565852@gmail.com>
2022-10-28 19:44:45 +0530

    add package manager interface

---
+++ src/project_generator/lib/toolchain_manager/_pkgmngr/_pkgmngrif.py
@@ -0,0 +1,73 @@
+'''
+Interface for package manager
+'''
+
+from enum import Enum
+
+from typing_extensions import Self
+
+
+class Action(Enum):
+    '''
+    An enum listing the supported actions for a package manager
+    '''
+    INSTALL = 'install'
+    REMOVE = 'remove'
+    UPDATE = 'update'
+
+
+class PackageManager:
+    '''
+    Abstraction to represent a package manager
+    '''
+
+    def __init__(self):
+        self.confirmation: bool = None
+        self.action: Action = None
+        self.pkglist: dict[str, Action] = {}
+        self.cmd_name: str = None
+
+    def confirm(self, cnf: bool = False) -> Self:
+        '''
+        Whether to ask confirmation for an action
+        '''
+        self.confirmation = cnf
+        return self
+
+    def install(self, pkglist: list[str]) -> Self:
+        '''
+        Install the given list of packages
+        '''
+        for pkg in pkglist:
+            val = self.pkglist.get(pkg)
+            if val is None:
+                self.pkglist[pkg] = Action.INSTALL
+            elif val == Action.REMOVE:
+                raise ValueError(
+                    f"Package {pkg} listed in at least 2 conflicting actions")
+        return self
+
+    def remove(self, pkglist: list[str]) -> Self:
+        '''
+        Remove the given list of packages
+        '''
+        for pkg in pkglist:
+            val = self.pkglist.get(pkg)
+            if val is None:
+                self.pkglist[pkg] = Action.REMOVE
+            elif val in [Action.INSTALL, Action.UPDATE]:
+                raise ValueError(
+                    f"Package {pkg} listed in at least 2 conflicting actions")
+        return self
+
+    def update(self, _: list[str] = None) -> Self:
+        '''
+        Update all or the selected list of packages
+        '''
+        return self
+
+    def run(self) -> int:
+        '''
+        Run the package manager commands
+        '''
+        return 0
