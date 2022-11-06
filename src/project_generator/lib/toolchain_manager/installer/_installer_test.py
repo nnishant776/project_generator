246d6635d5ffccfb52d467cd85801b0e61cf6b31
Neeraj Nishant <nn54565852@gmail.com>
2022-11-06 09:37:02 +0000

    add tests for installers

--- src/project_generator/lib/toolchain_manager/installer/_installer_test.py
+++ src/project_generator/lib/toolchain_manager/installer/_installer_test.py
@@ -22,6 +22,38 @@ class TestToolchainInstaller(unittest.TestCase):
             Distribution.FEDORA).install_toolchain(Toolchain.C).build()
         self.assertEqual(installer.install_toolchain(), 0)
 
+    def test_cpp_installer(self):
+        '''
+        Test the installation for C++ toolchain
+        '''
+        installer = ToolchainInstallerBuilder().distribution(
+            Distribution.FEDORA).install_toolchain(Toolchain.CPP).build()
+        self.assertEqual(installer.install_toolchain(), 0)
+
+    def test_gtk_installer(self):
+        '''
+        Test the installation for Gtk toolchain
+        '''
+        installer = ToolchainInstallerBuilder().distribution(
+            Distribution.FEDORA).install_toolchain(Toolchain.GTK).build()
+        self.assertEqual(installer.install_toolchain(), 0)
+
+    def test_go_installer(self):
+        '''
+        Test the installation for Go toolchain
+        '''
+        installer = ToolchainInstallerBuilder().distribution(
+            Distribution.FEDORA).install_toolchain(Toolchain.GO).build()
+        installer.install_toolchain()
+
+    def test_rust_installer(self):
+        '''
+        Test the installation for Rust toolchain
+        '''
+        installer = ToolchainInstallerBuilder().distribution(
+            Distribution.FEDORA).install_toolchain(Toolchain.RUST).build()
+        installer.install_toolchain()
+
 
 if __name__ == '__main__':
-    TestToolchainInstaller().test_c_installer()
+    unittest.main()
