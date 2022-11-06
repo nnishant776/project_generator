92031d68983ba298c2e1c6075e94ce72d8f4b072
Neeraj Nishant <nn54565852@gmail.com>
2022-11-06 09:35:20 +0000

    implement installer for Go toolchain

--- src/project_generator/lib/toolchain_manager/installer/_installer.py
+++ src/project_generator/lib/toolchain_manager/installer/_installer.py
@@ -3,12 +3,17 @@ Module for installing the configured toolchains for the project
 '''
 
 from dataclasses import dataclass
+from copy import deepcopy
+import os
+import tarfile
+import shutil
 
 from typing_extensions import Self
 
 from project_generator.lib.toolchain import Toolchain
 from project_generator.lib.distromngr import Distribution
 from project_generator.lib.pkgmngr import PackageManagerBuilder
+from ._util import _download_go_toolchain
 
 
 @dataclass(slots=True)
@@ -86,6 +91,43 @@ class _GtkToolchainInstaller(Installer):
 
 
 @dataclass(slots=True)
+class _GoToolchainInstaller(Installer):
+    '''
+    Go Toolchain installer
+    '''
+
+    def install_toolchain(self) -> int:
+        '''
+        Go toolchain installer
+        '''
+
+        go_tar_file = _download_go_toolchain()
+
+        go_root = os.getenv('GOROOT')
+        if go_root is None or go_root == "":
+            go_root = "/usr/local/sdks/go"
+
+        extract_path = "/tmp/golang"
+
+        tar_handler = tarfile.TarFile.gzopen(go_tar_file, 'r')
+        tar_handler.extractall(extract_path)
+
+        filenames = tar_handler.getnames()
+
+        extracted_path = deepcopy(extract_path)
+
+        common_path = os.path.commonpath(filenames)
+        if common_path.startswith(".") is False:
+            extracted_path = os.path.join(extracted_path, common_path)
+
+        shutil.rmtree(go_root, True, None)
+        os.makedirs(os.path.dirname(go_root), exist_ok=True)
+        shutil.move(extracted_path, go_root)
+
+        os.remove(go_tar_file)
+
+
+@dataclass(slots=True)
 class ToolchainInstallerBuilder:
     '''
     Get the installer based on inputs
@@ -121,5 +163,7 @@ class ToolchainInstallerBuilder:
             return _CppToolchainInstaller(self._distribution)
         elif self._toolchain == Toolchain.GTK:
             return _GtkToolchainInstaller(self._distribution)
+        elif self._toolchain == Toolchain.GO:
+            return _GoToolchainInstaller(self._distribution)
         else:
             return Installer(self._distribution)
---
+++ src/project_generator/lib/toolchain_manager/installer/_util.py
@@ -0,0 +1,67 @@
+'''
+Utility module used by the installer
+'''
+
+from urllib import request
+import json
+import os
+import tempfile
+
+
+def _download_go_toolchain(temp_output_path=None):
+    '''
+    Download the Go toolchain
+    '''
+
+    try:
+        with request.urlopen("https://go.dev/dl/?mode=json") as resp:
+            release_list = json.loads(resp.read())
+            latest_release = None
+            for release in release_list:
+                if release.get('stable', False):
+                    latest_release = release
+                    break
+
+            if latest_release is None:
+                raise ValueError("No stable release found")
+
+            version = latest_release.get('version', None)
+            if version is None:
+                raise ValueError(
+                    "Version information missing from release info")
+
+            files_info = latest_release.get('files', None)
+            if files_info is None:
+                raise ValueError(
+                    f"No files found in the latest release {version}")
+
+            release_file = None
+            for file_info in files_info:
+                if file_info.get('os', '') == "linux" and file_info.get('arch', "") == "amd64":
+                    release_file = file_info.get('filename', None)
+                    break
+
+            if release_file is None:
+                raise ValueError(
+                    f"No suitable file found in release {version}")
+
+            # get a valid output path, use temp path if not given
+            if temp_output_path is None or temp_output_path == "":
+                file_fd, temp_output_path = tempfile.mkstemp()
+                # close the return fd, as we are not going to use it
+                os.close(file_fd)
+
+            request.urlretrieve(
+                f"https://go.dev/dl/{release_file}", temp_output_path)
+
+            file_dir = os.path.dirname(temp_output_path)
+            final_output_path = os.path.join(file_dir, release_file)
+            os.rename(temp_output_path, final_output_path)
+            temp_output_path = final_output_path
+
+    except Exception as exc:
+        # clean up the downloaded temporary file in case of failures
+        os.remove(temp_output_path)
+        raise exc
+
+    return temp_output_path
