e2d05ec3ec0494d2c2d8e2b364dc8bf8483e477d
Neeraj Nishant <nn54565852@gmail.com>
2022-11-12 09:28:10 +0000

    update test for installer

--- src/project_generator/lib/toolchain_manager/installer/_installer_test.py
+++ src/project_generator/lib/toolchain_manager/installer/_installer_test.py
@@ -4,7 +4,7 @@ Test module for toolchain installer
 
 import unittest
 
-from project_generator.lib.distromngr import Distribution
+from project_generator.lib.distromngr import get_distribution
 from project_generator.lib.toolchain import Toolchain
 from project_generator.lib.toolchain_manager.installer import \
     ToolchainInstallerBuilder
@@ -19,41 +19,50 @@ class TestToolchainInstaller(unittest.TestCase):
         '''
         Test the installation for C toolchain
         '''
-        installer = ToolchainInstallerBuilder().distribution(
-            Distribution.FEDORA).install_toolchain(Toolchain.C).build()
-        self.assertEqual(installer.install_toolchain(), 0)
+
+        distro = get_distribution()
+        if distro is not None:
+            installer = ToolchainInstallerBuilder().distribution(
+                distro).install_toolchain(Toolchain.C).build()
+            self.assertEqual(installer.run(), 0)
 
     def test_cpp_installer(self):
         '''
         Test the installation for C++ toolchain
         '''
-        installer = ToolchainInstallerBuilder().distribution(
-            Distribution.FEDORA).install_toolchain(Toolchain.CPP).build()
-        self.assertEqual(installer.install_toolchain(), 0)
+
+        distro = get_distribution()
+        if distro is not None:
+            installer = ToolchainInstallerBuilder().distribution(
+                distro).install_toolchain(Toolchain.CPP).build()
+            self.assertEqual(installer.run(), 0)
 
     def test_gtk_installer(self):
         '''
         Test the installation for Gtk toolchain
         '''
-        installer = ToolchainInstallerBuilder().distribution(
-            Distribution.FEDORA).install_toolchain(Toolchain.GTK).build()
-        self.assertEqual(installer.install_toolchain(), 0)
+
+        distro = get_distribution()
+        if distro is not None:
+            installer = ToolchainInstallerBuilder().distribution(
+                distro).install_toolchain(Toolchain.GTK).build()
+            self.assertEqual(installer.run(), 0)
 
     def test_go_installer(self):
         '''
         Test the installation for Go toolchain
         '''
         installer = ToolchainInstallerBuilder().distribution(
-            Distribution.FEDORA).install_toolchain(Toolchain.GO).build()
-        installer.install_toolchain()
+            None).install_toolchain(Toolchain.GO).build()
+        self.assertEqual(installer.run(), 0)
 
     def test_rust_installer(self):
         '''
         Test the installation for Rust toolchain
         '''
         installer = ToolchainInstallerBuilder().distribution(
-            Distribution.FEDORA).install_toolchain(Toolchain.RUST).build()
-        installer.install_toolchain()
+            None).install_toolchain(Toolchain.RUST).build()
+        self.assertEqual(installer.run(), 0)
 
 
 if __name__ == '__main__':
